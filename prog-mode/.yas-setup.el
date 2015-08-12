bbc7dff458ffb89733c43e57516bc1b80161e15b
Henrik Lissner <henrik@lissner.net>
2015-08-12 17:14:30 +0200

    Update prog-mode snippets

--- prog-mode/.yas-setup.el
+++ prog-mode/.yas-setup.el
@@ -1,2 +1,16 @@
 (defun yas-with-comment (str)
   (format "%s%s%s" comment-start str comment-end))
+
+(defun yas-with-padded-comment (str)
+  (let* ((com-start (if (eq major-mode 'emacs-lisp-mode) ";; " comment-start))
+         (com-end (if (eq comment-end "") com-start comment-end)))
+    (format "%s%s%s%s" com-start str
+            (make-string (- fill-column (+ (length com-start) (length com-end) (length str))) ? )
+            com-end)))
+
+(defun yas-make-line ()
+  (let ((mid-char (string-to-char (if (equal comment-padding " ")  comment-start comment-padding))))
+    (format "%s%s%s" comment-start
+            (make-string (- fill-column (+ 1 (length comment-start) (length comment-end))) mid-char)
+            (or comment-end mid-char)
+            )))

7150ce3c5836383142260cb2d9da55c57f66b0d6
Henrik Lissner <henrik@lissner.net>
2015-09-30 03:22:50 -0400

    Put %-helper funcs into fundamental-mode/.yas-setup

--- fundamental-mode/.yas-setup.el
+++ fundamental-mode/.yas-setup.el
@@ -6,3 +6,23 @@
     (if (equal (cdr (car asslist)) query)
         (cons (car (car asslist)) (ca-all-asscs (cdr asslist) query))
       (ca-all-asscs (cdr asslist) query)))))
+
+(defun !%! ()
+  "Snippet function. Shorthand defun to surround text with newlines if more
+than one line."
+  (when %
+    (if (> (length (s-lines %)) 1)
+        (concat "\n" % "\n")
+      (s-trim %))))
+
+(defun !% ()
+  "Snippet function. Shorthand defun for snippets: prepends a newline to
+  `yas-selected-text' IF it contains more than one line."
+  (when %
+    (if (> (length (s-lines %)) 1)
+        (concat "\n" %)
+      (s-trim %))))
+
+(defun %1 ()
+  "Trim selection; do no further processing."
+  (s-trim %))

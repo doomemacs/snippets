f9ce95b49c98c9d3f71ae3846e1650104d60d65d
Henrik Lissner <henrik@lissner.net>
2017-11-04 22:29:26 +0100

    Refactor %emacs-lisp-evil-autoload fn

--- emacs-lisp-mode/.yas-setup.el
+++ emacs-lisp-mode/.yas-setup.el
@@ -4,22 +4,22 @@
 
 (defun %emacs-lisp-evil-autoload ()
   "Generate an autoload function for evil-define-* functions."
-  (when (file-in-directory-p buffer-file-name doom-modules-dir)
-    (let* ((sexp (or (save-excursion (beginning-of-line 2)
-                                     (thing-at-point 'sexp))
-                     ""))
-           (form (ignore-errors (read sexp))))
-      (when (and form
-                 (listp form)
-                 (memq (car form) '(evil-define-command evil-define-operator))
-                 (require 'autoload)
-                 (not (make-autoload form buffer-file-name)))
-        (format " (autoload '%s \"%s\" nil %s)"
-                (cadr form)
-                (file-relative-name
-                 (file-name-sans-extension buffer-file-name)
-                 (when (file-in-directory-p buffer-file-name doom-modules-dir)
-                   doom-modules-dir))
-                (if (cl-some (lambda (x) (eq (if (listp x) (car x) x) 'interactive)) form)
-                    "t"
-                  "nil"))))))
+  (let* ((sexp (or (save-excursion (beginning-of-line 2)
+                                   (thing-at-point 'sexp))
+                   ""))
+         (form (ignore-errors (read sexp))))
+    (when (and form
+               (listp form)
+               (memq (car form) '(evil-define-command evil-define-operator))
+               (require 'autoload)
+               (not (make-autoload form buffer-file-name)))
+      (format " (autoload '%s \"%s\" nil %s)"
+              (cadr form)
+              (file-relative-name
+               (file-name-sans-extension buffer-file-name)
+               (if (file-in-directory-p buffer-file-name doom-modules-dir)
+                   doom-modules-dir
+                 (doom-project-root)))
+              (if (cl-some (lambda (x) (eq (if (listp x) (car x) x) 'interactive)) form)
+                  "t"
+                "nil")))))

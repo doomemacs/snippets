91bc39213675ad299c7b44f028d7a68d1925884d
Henrik Lissner <henrik@lissner.net>
2019-10-26 02:26:31 -0400

    Move snippets out of snippets subdirectory
    Why? Because it easy to use this repo as a drop-in snippets library for
    doom users (e.g. cloning directly to '~/.doom.d/snippets'). The other
    way required some shenanigans, possibly involving symlinks.

--- doom-snippets.el
+++ doom-snippets.el
@@ -20,13 +20,11 @@
 ;;; Code:
 
 (defvar doom-snippets-dir
-  (expand-file-name
-   "snippets"
-   (file-name-directory
-    (cond (load-in-progress load-file-name)
-          ((bound-and-true-p byte-compile-current-file)
-           byte-compile-current-file)
-          (buffer-file-name))))
+  (file-name-directory
+   (cond (load-in-progress load-file-name)
+         ((bound-and-true-p byte-compile-current-file)
+          byte-compile-current-file)
+         (buffer-file-name)))
   "The base directory of the doom-snippets library.")
 
 (defvar doom-snippets-enable-short-helpers nil
